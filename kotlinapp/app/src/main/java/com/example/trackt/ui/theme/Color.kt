d1fe5f90cd309ed33caf6c17bc9ea584867972c7
Oluwanifemi Daramola <76186151+nifedara@users.noreply.github.com>
2023-11-02 11:35:47 +0100

    Initial commit

---
+++ kotlinapp/app/src/main/java/com/example/trackt/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.trackt.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val TracktPurple1 = Color(0xFF7D1679)
+val TracktPurple2 = Color(0xFF800854)
+val TracktPurple3 = Color(0xFFDDC3DC)
+
+val TracktBlue1 = Color(0xFF0D1450)
+val TracktBlue2 = Color(0xFF56ACDC)
+
+val TracktWhite1 = Color(0xFFFDFDFD)
+val TracktGray1 = Color(0xFFF0F0F0)
