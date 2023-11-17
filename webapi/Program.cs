7e17f134bdd7186414cf5f6105de1198e9b7403f
Oluwanifemi Daramola <76186151+nifedara@users.noreply.github.com>
2023-11-17 13:20:16 +0100

    account endpoints

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using webapi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
